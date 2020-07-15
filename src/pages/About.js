e3e9f3a9dcd42e13c9d893e4c5d97ba92eda3b3d
Lucien Greathouse <me@lpghatguy.com>
2020-07-14 23:17:47 -0700

    Move AboutContent.mdx

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,8 @@ import React from "react";
 import { Helmet } from "react-helmet";
 
 import PageBody from "../PageBody";
-import AboutContent from "../AboutContent.mdx";
+
+import AboutContent from "./AboutContent.mdx";
 
 export default function About() {
 	return (
